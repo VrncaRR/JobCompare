2281e04834e2dfb45fc63ce23738827170377300
Luke Runyan <lrunyan3@gatech.edu>
2023-03-02 13:35:39 -0500

    Merged UI_Dev and GP03

---
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/jobListAdapter.java
@@ -0,0 +1,13 @@
+package edu.gatech.seclass.jobcompare6300;
+
+import androidx.recyclerview.widget.RecyclerView;
+/*
+public class jobListAdapter extends RecyclerView.Adapter<jobListAdapter.jobListHolder> {
+
+
+    class jobListHolder extends RecyclerView.ViewHolder{
+        private TextView textView;
+        private ImageView imageView;
+    }
+}
+*/
