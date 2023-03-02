607f6412a43265dfef759462c75076ce97328347
rfang <rfang40@gatech.edu>
2023-03-02 01:18:29 -0600

    add some backend

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/CurrentJob.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/CurrentJob.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CurrentJob extends AppCompatActivity {
     //Declare EditText variables
@@ -20,6 +21,9 @@ public class CurrentJob extends AppCompatActivity {
     private EditText entryCurJobRelo;
     private EditText entryCurJobPCH;
     private TextView testingTextView;
+    private Job currentJob = null;
+
+    DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,25 @@ public class CurrentJob extends AppCompatActivity {
         entryCurJobPCH = (EditText) findViewById(R.id.entryCurJobPCH);
         testingTextView = (TextView) findViewById(R.id.testingTextView);
 
+        //get current job
+        dbHelper = new DatabaseHelper(CurrentJob.this);
+        currentJob = dbHelper.getCurrentJob();
+
+        //set current job attributes
+        if(currentJob!=null) {
+            entryCurJobTitle.setText(currentJob.getTitle());
+            entryCurJobCompany.setText(currentJob.getCompany());
+            entryCurJobLocation.setText(currentJob.getLocation());
+            entryCurJobCOL.setText(String.valueOf(currentJob.getCostOfLiving()));
+            entryCurJobSalary.setText(String.valueOf(currentJob.getYearlySalary()));
+            entryCurJobBonus.setText(String.valueOf(currentJob.getYearlyBonus()));
+            entryCurJobRSU.setText(String.valueOf(currentJob.getRsu()));
+            entryCurJobRelo.setText(String.valueOf(currentJob.getRelocationStipend()));
+            entryCurJobPCH.setText(String.valueOf(currentJob.getPto()));
+
+        }
+
+
         configureCancelJobButton();
         configureSaveCurJobButton();
         handleSaveCurJob();
@@ -49,20 +72,58 @@ public class CurrentJob extends AppCompatActivity {
         SaveCurJobButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String curJobTitle = entryCurJobTitle.getText().toString();
-                String curJobCompany = entryCurJobCompany.getText().toString();
-                String curJobLocation = entryCurJobLocation.getText().toString();
-                String curJobCOL = entryCurJobCOL.getText().toString();
-                String curJobSalary = entryCurJobSalary.getText().toString();
-                String curJobBonus = entryCurJobBonus.getText().toString();
-                String curJobRSU = entryCurJobRSU.getText().toString();
-                String curJobRelo = entryCurJobRelo.getText().toString();
-                String curJobPCH = entryCurJobPCH.getText().toString();
 
+                String ERROR_MESSAGE = "Invalid Entry";
                 Boolean error = false;
-                //TODO: ADD Error Handling for Invalid Entries
 
-                //TODO: Add create Current Job actions here
+                try{
+                    String offerTitle = entryCurJobTitle.getText().toString();
+                    String offerCompany = entryCurJobCompany.getText().toString();
+                    String offerLocation = entryCurJobLocation.getText().toString();
+                    int offerCOL = Integer.parseInt(entryCurJobCOL.getText().toString());
+                    float offerSalary = Float.parseFloat(entryCurJobSalary.getText().toString());
+                    float offerBonus = Float.parseFloat(entryCurJobBonus.getText().toString());
+                    float offerRSU = Float.parseFloat(entryCurJobRSU.getText().toString());
+                    float offerRelo = Float.parseFloat(entryCurJobRelo.getText().toString());
+                    int offerPCH = Integer.parseInt(entryCurJobPCH.getText().toString());
+
+                    if (offerTitle.length() == 0) {
+                        entryCurJobTitle.setError(ERROR_MESSAGE);
+                        error = true;
+                    }
+                    if (offerCompany.length() == 0) {
+                        entryCurJobCompany.setError(ERROR_MESSAGE);
+                        error = true;
+                    }
+                    if (offerLocation.length() == 0) {
+                        entryCurJobLocation.setError(ERROR_MESSAGE);
+                        error = true;
+                    }
+                    if (!error) {
+                        //TODO: Add create Current Job actions here
+                        Job newOffer = new Job(offerTitle, offerCompany, offerLocation,
+                                offerCOL, offerSalary, offerBonus, offerRSU, offerRelo, offerPCH, true);
+
+
+                        //check if current job is created, if so, update current job, else create new current job
+                        if(currentJob!= null) {
+                            //update current job
+                            if(dbHelper.updateCurrentJob(newOffer)) {
+                                Toast.makeText(CurrentJob.this, "Successfully update current Job!", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(CurrentJob.this, "fail to add current job to db", Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                        else if(dbHelper.addJobOffer(newOffer)) {
+                            Toast.makeText(CurrentJob.this, "Successfully modify the current job!", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(CurrentJob.this, "fail to add db", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }  catch(Exception e) {
+                    Toast.makeText(CurrentJob.this, "Unable to add new job offer, please check input!", Toast.LENGTH_LONG).show();
+                }
 
             }
         });
